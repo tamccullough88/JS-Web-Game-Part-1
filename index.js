c3fe63933ec75d17e138b77613c929d179e91fb6
tamccullough88 <tamccullough88@gmail.com>
2023-08-29 20:54:40 -0400

    added grass and sky to the background, put screenshot in the assets folder for proof of how it looks on my screen

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
+//background images. my inner window size is 1012w x 875h
+
+function background(url, left, bottom, width, height){
+    for(let h = 0; h < height; h++){
+        for(let w = 0; w < width; w++){
+            newImage(url, left + w*100, bottom+h*100)
+        }
+    }
+}
+
+
+
+// images and "objects"
+
 function newImage(url, left, bottom){
     let object = document.createElement('img')
     object.src = url
@@ -8,6 +22,8 @@ function newImage(url, left, bottom){
     return object
 }
 
+//item function, set listener so that we can pick up and interact with the items when double clicked
+
 function newItem(url, left, bottom){
     let object = newImage(url, left, bottom)
 
@@ -16,6 +32,16 @@ function newItem(url, left, bottom){
     })
 }
 
+let horizon = window.innerWidth / 3.5
+let heightOfSky = window.innerHeight-horizon
+let heightOfGrass = horizon
+
+//background
+background('assets/sky.png', 0, horizon, window.innerWidth/100, heightOfSky/100)
+background('assets/grass.png', 0, 0,window.innerWidth/100, heightOfGrass/100)
+
+// images 
+
 newImage('assets/green-character.gif', 100, 100)
 newImage('assets/tree.png', 200, 300)
 newImage('assets/pillar.png', 350, 100)
@@ -23,6 +49,9 @@ newImage('assets/pine-tree.png', 450, 200)
 newImage('assets/crate.png', 150, 200)
 newImage('assets/well.png', 500, 425)
 
+//interactable items
+
 newItem('assets/sword.png', 500, 405)
 newItem('assets/shield.png', 165, 185)
 newItem('assets/staff.png', 600, 100)
+
